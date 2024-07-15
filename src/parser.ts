a5202996eb6b37ab6dcb95fcc02ec329e7e6c154
Eduardo Vasconcelos <erodrigues@nic.br>
2024-07-15 16:12:40 -0300

    2o commit - srp

--- src/parser.ts
+++ src/parser.ts
@@ -1,97 +1,38 @@
+import FileConverter from "./services/file.converter";
+import FileReader from "./services/file-reader";
+import FileWriter from "./services/file-writer";
+
 export class Parser {
+  public sourceContent: string = "";
+
+  protected converterFile: FileConverter;
+  protected writerFile: FileWriter;
+  protected readerFile: FileReader;
+
   public static TYPE = {
     YAML: "yaml",
     JSON: "json",
     XML: "xml",
   };
 
-  public constructor(protected type: string, protected fileName: string) {}
-  public sourceContent: string = "";
-
-  protected yamlToJson(): string {
-    this.loadFile(this.fileName);
-    const content = `Convert content from ${this.type}:${this.fileName} to json`;
-    return content;
-  }
-
-  protected yamlToXml(): string {
-    this.loadFile(this.fileName);
-    const content = `Convert content from ${this.type}:${this.fileName} to xml`;
-    return content;
-  }
-
-  protected jsonToYaml(): string {
-    this.loadFile(this.fileName);
-    const content = `Convert content from ${this.type}:${this.fileName} to yaml`;
-    return content;
-  }
-
-  protected jsonToXml(): string {
-    this.loadFile(this.fileName);
-    const content = `Convert content from ${this.type}:${this.fileName} to xml`;
-    return content;
-  }
-
-  protected xmlToJson(): string {
-    this.loadFile(this.fileName);
-    const content = `Convert content from ${this.type}:${this.fileName} to json`;
-    return content;
-  }
-
-  protected xmlToYaml(): string {
-    this.loadFile(this.fileName);
-    const content = `Convert content from ${this.type}:${this.fileName} to yaml`;
-    return content;
+  public constructor(protected type: string, protected fileName: string) {
+    this.converterFile = new FileConverter();
+    this.readerFile = new FileReader();
+    this.writerFile = new FileWriter();
   }
 
   public convert(destType: string, destFileName: string): boolean {
-    let content = "";
-    if (this.type === Parser.TYPE.YAML && destType === Parser.TYPE.JSON) {
-      content = this.yamlToJson();
+    // Só para não carregar o arquivo novamente, se ele for muito grande pode ser um problema
+    if (this.sourceContent === "") {
+      this.sourceContent = this.readerFile.loadFile(this.fileName);
     }
 
-    if (this.type === Parser.TYPE.YAML && destType === Parser.TYPE.XML) {
-      content = this.yamlToXml();
-    }
-
-    if (this.type === Parser.TYPE.JSON && destType === Parser.TYPE.YAML) {
-      content = this.jsonToYaml();
-    }
-
-    if (this.type === Parser.TYPE.JSON && destType === Parser.TYPE.XML) {
-      content = this.jsonToXml();
-    }
-
-    if (this.type === Parser.TYPE.XML && destType === Parser.TYPE.JSON) {
-      content = this.xmlToJson();
-    }
-
-    if (this.type === Parser.TYPE.XML && destType === Parser.TYPE.YAML) {
-      content = this.xmlToYaml();
-    }
-
-    if (content === "") {
-      return false;
-    }
-
-    console.log(
-      `Convert content from ${this.type}:${this.fileName} to ${destType}:${destFileName}`
+    const content = this.converterFile.convert(
+      this.type,
+      this.sourceContent,
+      destType
     );
-    this.writeFile(destFileName, content);
-    return true;
-  }
 
-  protected loadFile(fileName: string): string {
-    if (this.sourceContent !== "") {
-      return this.sourceContent;
-    }
-
-    this.sourceContent = `Load file ${fileName}`;
-    return this.sourceContent;
-  }
-
-  protected writeFile(fileName: string, content: string): boolean {
-    console.log(`Write content to file ${fileName}`);
-    return true;
+    return this.writerFile.writeToFile(destFileName, content);
   }
 }
